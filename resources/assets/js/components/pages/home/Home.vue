ed5c631d8e64f24e18ee143df9c80ccfa7a9503f
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-14 19:18:33 +0430

    vue home page modify

---
+++ resources/assets/js/components/pages/home/Home.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="row">
+        <header class="col-xs-12">
+            <iconbar-top></iconbar-top>
+        </header>
+        <main class="col-xs-12">
+            <div class="main-menu row">
+                <div class="col-md-12">
+                    <img src="/img/app.png" class="app-logo" />
+                    <router-link to="dash" class="shortcut">
+                        <span class="tag"></span>
+                        <span class="caption">DASHBOARD</span>
+                        <span class="mif-dashboard mif-5x"></span>
+                    </router-link>
+                    <router-link to="" class="shortcut">
+                        <span class="tag">14</span>
+                        <span class="caption">INBOX</span>
+                        <span class="mif-envelop mif-5x"></span>
+                    </router-link>
+                    <router-link to="" class="shortcut">
+                        <span class="tag"></span>
+                        <span class="caption">H.R</span>
+                        <span class="mif-users mif-5x"></span>
+                    </router-link>
+                    <router-link to="" class="shortcut">
+                        <span class="tag">3</span>
+                        <span class="caption">REQUESTS</span>
+                        <span class="mif-user-plus mif-5x"></span>
+                    </router-link>
+                    <router-link to="" class="shortcut">
+                        <span class="tag"></span>
+                        <span class="caption">TIMESHEETS</span>
+                        <span class="mif-alarm mif-5x"></span>
+                    </router-link>
+                    <router-link to="" class="shortcut">
+                        <span class="tag">2</span>
+                        <span class="caption">SCHEDULE</span>
+                        <span class="mif-calendar mif-5x"></span>
+                    </router-link>
+                    <router-link to="" class="shortcut">
+                        <span class="tag"></span>
+                        <span class="caption">SETTINGS</span>
+                        <span class="mif-cog mif-5x"></span>
+                    </router-link>
+                </div>
+            </div>
+        </main>
+        <footer class="col-xs-12">
+            <iconbar-bot></iconbar-bot>
+        </footer>
+    </div>
+</template>
+
+<script>
+import IconbarTop from "../../framework/iconbar/IconbarTop.vue";
+export default {
+  components: {
+    "iconbar-top": IconbarTop
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
