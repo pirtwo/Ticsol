7ecd022e748b32ebafe1ee5fd820d2b897fd52aa
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-22 11:30:56 +0430

    add user.php to models

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $primaryKey = 'user_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_name', 'user_email', 'user_password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'user_password', 'remember_token',
+    ];
+}
