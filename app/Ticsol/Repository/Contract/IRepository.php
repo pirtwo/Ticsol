8ff7694ec44a66c66997738bbd1b4a0fa028cca6
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-23 11:21:47 +0430

    create IRepository.php

---
+++ app/Ticsol/Repository/Contract/IRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Ticsol\Repository;
+
+interface Repository{
+    public function all();
+    public function find($id, $columns = array('*'));
+    public function findBy($field, $value, $columns = array('*'));
+    public function paginate($perPage = 15, $columns = array('*'));
+    public function create(array $data);
+    public function update(array $data, $id);
+    public function delete($id);
+}
