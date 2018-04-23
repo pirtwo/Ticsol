81ced709ea34e978af766be3f4a048eddcbbf00c
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-23 20:20:52 +0430

    implement icriteria.php by repository.php

--- app/Ticsol/Repository/Base/Repository.php
+++ app/Ticsol/Repository/Base/Repository.php
@@ -2,40 +2,53 @@
 
 namespace App\Ticsol\Repository\Base;
 
+use App\Ticsol\Repository\Contract\ICriteria;
 use App\Ticsol\Repository\Contract\IRepository;
 use Illuminate\Container\Container as App;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 /**
- * Concret class for eloquent model repository.
+ * Base class for eloquent model repository.
  */
-abstract class Repository implements IRepository
+abstract class Repository implements IRepository, ICriteria
 {
 
     /**
      * Instance of Illuminate\Container\Container
+     * @var App
      */
     private $app;
 
     /**
      * Eloquent model instance.
+     * @var
      */
     protected $model;
 
+    /**
+     * @var Collection
+     */
+    protected $criterias;
 
-    public function __construct(App $app)
+    /**
+     * @var bool
+     */
+    protected $skipCriteria;
+
+    public function __construct(App $app, Collection $collection)
     {
         $this->app = $app;
+        $this->criteria = $collection;
+
         $this->makeModel();
     }
-
 
     /**
      * Defines eloquent model class name.
      * @return mixed
      */
     abstract public function model();
-
 
     /**
      * Creates instance of Model.
@@ -48,26 +61,24 @@ abstract class Repository implements IRepository
         if (!$model instanceof Model) {
             throw new \Exception("Class {$this->model()} must be an instance of Illuminate\\Database\\Eloquent\\Model");
         }
-        
+
         return $this->model = $model->newQuery();
     }
 
-
     /**
      * @param array $columns
-     * 
+     *
      * @return mixed
      */
     public function all($columns = array('*'))
-    {        
+    {
         return $this->model->get($columns);
     }
-
 
     /**
      * @param $id
      * @param array $columns
-     * 
+     *
      * @return mixed
      */
     public function find($id, $columns = array('*'))
@@ -75,12 +86,11 @@ abstract class Repository implements IRepository
         return $this->model->find($id, $columns);
     }
 
-
     /**
      * @param $field
      * @param $value
      * @param array $columns
-     * 
+     *
      * @return mixed
      */
     public function findBy($field, $value, $columns = array('*'))
@@ -88,11 +98,10 @@ abstract class Repository implements IRepository
         return $this->model->where($field, '=', $value)->first($columns);
     }
 
-
     /**
      * @param int $perPage
      * @param array $columns
-     * 
+     *
      * @return mixed
      */
     public function paginate($perPage = 15, $columns = array('*'))
@@ -100,10 +109,9 @@ abstract class Repository implements IRepository
         return $this->model->paginate($perPage, $columns);
     }
 
-
     /**
      * @param array $data
-     * 
+     *
      * @return mixed
      */
     public function create(array $data)
@@ -111,12 +119,11 @@ abstract class Repository implements IRepository
         return $this->model->create($data);
     }
 
-
     /**
      * @param array $data
      * @param string $field
      * @param string $value
-     * 
+     *
      * @return mixed
      */
     public function update(array $data, $field, $value)
@@ -124,14 +131,78 @@ abstract class Repository implements IRepository
         return $this->model->where($field, '=', $value)->update($data);
     }
 
-
     /**
      * @param $id
-     * 
+     *
      * @return mixed
      */
     public function delete($id)
     {
         return $this->model->destroy($id);
+    }
+
+    /**
+     * @return $this
+     */
+    public function resetScope()
+    {
+        $this->skipCriteria(false);
+        return $this;
+    }
+
+    /**
+     * @param bool $status
+     * @return $this
+     */
+    public function skipCriteria($status = true)
+    {
+        $this->skipCriteria = $status;
+        return $this;
+    }
+
+    /**
+     * @param Criteria $criteria
+     * @return $this
+     */
+    public function pushCriteria(Criteria $criteria)
+    {
+        $this->criterias->push($criteria);
+        return $this;
+    }
+
+    /**
+     * @param Criteria $criteria
+     * @return $this
+     */
+    public function getByCriteria(Criteria $criteria)
+    {
+        $this->model = $criteria->aplly($this->model, $this);
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCriteria()
+    {
+        return $this->criterias;
+    }
+
+    /**
+     * @return $this
+     */
+    public function applyCriteria()
+    {
+        if ($this->skipCriteria) {
+            return $this;
+        }
+
+        foreach ($this->getCriteria() as $critaria) {
+            if ($critaria instanceof Criteria) {
+                $this->model = $critaria->apply($this->model, $this);
+            }
+        }
+
+        return $this;
     }
 }
