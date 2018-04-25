7f38c1c64e5ef92b19629aee9c7244220cffcc3d
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-25 09:56:11 +0430

    add contact.php to models

---
+++ app/Ticsol/Models/Contact.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Ticsol\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Ticsol\Models;
+
+class Contact extends Model
+{    
+    protected $primaryKey = 'contact_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'contact_group',
+        'contact_firstname',
+        'contact_lastname',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+}
