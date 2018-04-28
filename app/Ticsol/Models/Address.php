870432b56936a3742b97d8b2a4310faa6247528b
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-28 19:16:27 +0430

    add address.php to models

---
+++ app/Ticsol/Models/Address.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Ticsol\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Ticsol\Models;
+
+class Address extends Model
+{    
+    protected $table = 'ts_Addresses';
+    protected $primaryKey = 'address_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'address_unit',
+        'address_street',
+        'address_suburb',
+        'address_country',
+        'address_postcode',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+}
