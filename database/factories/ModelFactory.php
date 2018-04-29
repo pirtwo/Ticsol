1a908cd65ff07f7b3a030a7f7f2866a11401ddf7
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-29 19:14:56 +0430

    modify user factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,13 +12,13 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Ticsol\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'user_name' => $faker->name,
+        'user_email' => $faker->unique()->safeEmail,
+        'user_password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
 });
