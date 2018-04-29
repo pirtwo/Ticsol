4819bfc4b84eb3257cc3521e80bf880baccb23f8
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-04-29 09:57:29 +0430

    add login method to loginController.php

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\api\auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $data = [
+            'grant_type' => 'password',
+            'client_id'  => env('Password_Client_ID'),
+            'client_secret' => env('Password_Client_Secret'),
+            'username' => $request->get('username'),
+            'password' => $request->get('password')
+        ];
+
+        $new_req = Request::create('/oauth/token', 'POST', $data);
+    }
 }
