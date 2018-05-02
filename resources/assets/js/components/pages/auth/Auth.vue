597350d85291b143ad018cc86968bfdf4eb182c2
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-05-02 10:19:45 +0430

    add vue auth components

---
+++ resources/assets/js/components/pages/auth/Auth.vue
@@ -0,0 +1,19 @@
+<template>
+     <div class="row">
+        <header class="col-xs-12">                      
+        </header>
+        <main class="col-xs-12">
+            <router-view></router-view>
+        </main>
+        <footer class="col-xs-12">                        
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/pages/auth/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="">
+      <form method="POST" v-on:submit.prevent="onSubmit">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="email" placeholder="Enter username"/>                
+            </div>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" placeholder="Enter password"/>
+                <small class="text-muted">Never share your password in public.</small>
+            </div>
+            <div class="form-group">
+                <input type="checkbox" data-role="checkbox" data-caption="Remember me">
+            </div>
+            <div class="form-group">
+                <button class="button success">Login</button>                
+            </div>
+      </form>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      axios.post("https://server.dev/api/login", {
+        body: [this.username, this.password]
+      });
+    }
+  }
+};
+</script>
+<style scoped>
+
+</style>
