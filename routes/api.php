8346b074f1edf91f7928dd33c945bb1249a3833b
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-08-06 13:15:43 +0430

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -14,35 +14,32 @@ use Illuminate\Http\Request;
  */
 
 Route::group([ 'prefix' => '', 'namespace' => 'App\Ticsol\Components\Controllers\API'], function(){
-        
-    #region Method: GET
-    // Users
-    Route::get('user/list', 'UserController@get');
-    Route::get('user/client/{id}', 'UserController@client');
-    
-    // Jobs
-    Route::get('job/list', 'JobController@list');
-    Route::get('job/view/{id}', 'JobController@view');   
-    
-    // Schedule
-    Route::get('schedule/list', 'ScheduleController@list');    
-    #endregion
-
-    #region Method: POST
+            
     // Auth
     Route::post('/login', 'AuthController@login');
     Route::post('/refresh', 'AuthController@refresh');
 
-    // Jobs    
-    Route::post('job/create', 'JobController@create');
-    Route::post('job/update/{id}', 'JobController@update'); 
+    // Users
+    Route::get('user', 'UserController@index');
+        
+    // Jobs
+    Route::get('job', 'JobController@index');
+    Route::get('job/show/{id}', 'JobController@show'); 
+    Route::post('job/store', 'JobController@store');
+    Route::post('job/update/{id}', 'JobController@update');   
     
     // Schedule
-    Route::post('schedule/create', 'ScheduleController@create');  
-    Route::post('schedule/update/{id}', 'ScheduleController@update');   
-    #endregion
-
-
+    Route::get('schedule', 'ScheduleController@index'); 
+    Route::get('schedule/show/{id}', 'ScheduleController@show'); 
+    Route::post('schedule/store', 'ScheduleController@store');  
+    Route::post('schedule/update/{id}', 'ScheduleController@update');  
+    
+    // Activity
+    Route::get('activity', 'ActivityController@index');
+    Route::get('activity/show/{id}', 'ActivityController@show');
+    Route::post('activity/store', 'ActivityController@store');
+    Route::post('activity/update/{id}', 'ActivityController@update');
+    
     /**
      * Protected api routes
      */
