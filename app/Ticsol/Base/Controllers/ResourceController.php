a2b9433a4ac058b168a57e80d7994290c1166ab6
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-08-15 11:44:10 +0430

    add resource controller

---
+++ app/Ticsol/Base/Controllers/ResourceController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Ticsol\Base\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use App\Ticsol\Base\Repository;
+
+class ResourceController extends Controller
+{
+    /**
+     * Resource base repository.
+     * @var App\Ticsol\Base\Repository
+     */
+    protected $repository;
+
+    /**
+     * Resource eloquent model used in controller.
+     * @var Illuminate\Database\Eloquent\Model
+     */
+    protected $model;
+
+    /**
+     * Model not found exception.
+     * @var Exception
+     */
+    protected $notFoundExp;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
