b277fc2f567ecd7e421f2d85cacfd69814ed25d0
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-10-13 13:22:48 +0330

    add not found exception to base

---
+++ app/Ticsol/Base/Exceptions/NotFound.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Ticsol\Components\Base\Exceptions;
+
+use Exception;
+
+class NotFound extends Exception
+{
+    /**
+     * Report the exception.
+     *
+     * @return void
+     */
+    public function report()
+    {
+        //
+    }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        return response()->json(
+            [
+                'error' => true,
+                'code' => 404,
+                'message' => 'The resource you are looking for could not be found.',
+            ], 404);
+    }
+}
