7ffd503f515347f82626462085371dcb3ff55104
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2018-11-02 10:52:15 +0330

    vue add role pages

---
+++ resources/assets/js/components/pages/dashboard/roles/RoleList.vue
@@ -0,0 +1,138 @@
+<template>
+    <nav-view :scrollbar="true" :loading="loading" padding="p-2">
+
+        <template slot="toolbar">
+          <button type="button" @click="showFilter = true">Filter</button>
+          <pagination-view v-model="pager" :page-count="pager.pageCount"></pagination-view>
+        </template>
+
+        <template slot="drawer">
+            <ul class="v-menu">
+                <li class="menu-title">Actions</li>
+                <li><router-link :to="{ name: 'roleCreate' }">New</router-link></li>
+                <li class="menu-title">Links</li>                
+            </ul>
+        </template>
+
+        <template slot="content">
+
+            <table-view class="table table-striped" 
+            v-model="selects" :data="jobs" :header="header" :selection="false" order-by="title" order="asc">
+               <template slot="header" slot-scope="{item}">
+                 <div :data-orderBy="item.orderBy">{{ item.value }}</div>
+               </template>
+               <template slot="body" slot-scope="{item}">
+                 <td>
+                  <router-link class="btn btn-sm btn-light" :to="{ name : 'roleDetails', params : { id: item.id } }">
+                    <i class="material-icons">visibility</i>
+                  </router-link> 
+                 </td>
+                 <td>{{ item.name }}</td>
+                 <td>{{ item.created_at }}</td>
+                 <td>{{ item.updated_at }}</td>
+               </template> 
+            </table-view>
+            <filter-view v-model="query" :show.sync="showFilter" :columns="columnList" @apply="feedTable"></filter-view>
+
+        </template>
+    </nav-view>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import FilterView from "../../../framework/BaseFilter.vue";
+import NavView from "../../../framework/NavView.vue";
+import TableView from "../../../framework/BaseTable.vue";
+import PaginationView from "../../../framework/BasePagination.vue";
+
+export default {
+  name: "RoleList",
+
+  components: {
+    "nav-view": NavView,
+    "table-view": TableView,
+    "filter-view": FilterView,
+    "pagination-view": PaginationView
+  },
+
+  props: ["col", "opt", "val"],
+
+  data() {
+    return {
+      jobs: [],
+      pager: {
+        page: 1,
+        perPage: 10,
+        pageCount: 10
+      },
+      loading: true,
+      selects: [],
+      query: [],
+      showFilter: false,
+      header: [
+        { value: "", orderBy: "" },
+        { value: "Title", orderBy: "name" },
+        { value: "Created At", orderBy: "created_at" },
+        { value: "Updated At", orderBy: "updated_at" }
+      ],
+      order: "asc"
+    };
+  },
+
+  watch: {
+    pager: function(value) {
+      this.feedTable();
+    },
+
+    query: function(value) {
+      console.log("update");
+      console.log(value);
+    }
+  },
+
+  computed: {
+    columnList: function() {
+      return [
+        { key: "title", value: "Title" },
+        { key: "code", value: "Code" },
+        { key: "isactive", value: "Active" }
+      ];
+    }
+  },
+
+  mounted() {    
+    this.feedTable();
+  },
+
+  methods: {
+    ...mapActions({ fetch: "resource/list", logger: "core/pushLog" }),
+
+    feedTable() {
+      this.loading = true;
+      this.fetch({
+        resource: "role",
+        query: this.$queryBuilder(
+          this.pager.page,
+          this.pager.perPage,
+          this.opt,
+          this.col,
+          this.val,
+          this.query
+        )
+      })
+        .then(respond => {
+          this.jobs = respond.data;
+          this.pager.pageCount = respond.last_page;
+          this.loading = false;
+        })
+        .catch(error => {
+          console.log(error);
+          this.loading = false;
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ resources/assets/js/components/pages/dashboard/roles/RoleUsers.vue
@@ -0,0 +1,212 @@
+<template>
+    <nav-view :scrollbar="true" :loading="loading" padding="p-5">
+
+        <template slot="toolbar"></template>
+
+        <template slot="drawer">
+
+            <ul class="v-menu">
+                <li class="menu-title">Actions</li>
+                <li>
+                    <button class="btn btn-light" @click="onSave">
+                        New
+                    </button>
+                </li>
+                <li>
+                    <button class="btn btn-light" @click="onSave">
+                        Save
+                    </button>
+                </li>
+                <li>
+                    <button class="btn btn-light" @click="onCancel">
+                        Cancel
+                    </button>
+                </li>
+                <li class="menu-title">Links</li>
+                <li>
+                    <router-link :to="{ name: 'roleList' }">Role Maintenance</router-link>
+                </li>
+            </ul>
+
+        </template>
+
+        <template slot="content">
+
+            <form class="needs-validation">
+
+                <div class="form-group">
+                    <div class="form-row">
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <label class="col-sm-2 col-form-label">Name</label>
+                                <div class="col-sm-10">
+
+                                    <input v-model="form.name" 
+                                    id="name" type="text" class="form-control" placeholder="role name" readonly />
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <label class="col-sm-2 col-form-label">Users</label>
+                                <div class="col-sm-10">
+                                    <select-box v-model="form.users" :data="userList" :multi-select="true" id="users"
+                                        name="users" placeholder="select users..." search-placeholder="search..."></select-box>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <table-view class="table table-striped" :data="roleUsers" :header="header" :selection="false"
+                    order-by="title" order="asc">
+                    <template slot="header" slot-scope="{item}">
+                        <div :data-orderBy="item.orderBy">{{ item.value }}</div>
+                    </template>
+                    <template slot="body" slot-scope="{item}">
+                        <td>
+                            <router-link class="btn btn-sm btn-light" 
+                            :to="{ name : 'userDetails', params : { id: item.id } }">
+                                <i class="material-icons">visibility</i>
+                            </router-link>
+                        </td>
+                        <td>{{ item.name }}</td>
+                        <td>
+                            <router-link 
+                            v-for="item in item.roles" :key="item.id"
+                            class="btn btn-sm btn-link" 
+                            :to="{ name : 'roleDetails', params : { id: item.id } }">
+                                {{ item.name }}
+                            </router-link>
+                        </td>
+                    </template>
+                </table-view>
+                
+
+            </form>
+
+        </template>
+    </nav-view>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import NavView from "../../../framework/NavView.vue";
+import Selectbox from "../../../framework/BaseSelectBox.vue";
+import TableView from "../../../framework/BaseTable.vue";
+
+export default {
+  name: "RoleUsers",
+
+  components: {
+    "nav-view": NavView,
+    "select-box": Selectbox,
+    "table-view": TableView
+  },
+
+  props: ["id"],
+
+  data() {
+    return {
+      currentRole: null,
+      form: {
+        name: "",
+        users: []
+      },
+      header: [
+        { value: "", orderBy: "" },
+        { value: "Name", orderBy: "name" },
+        { value: "Roles", orderBy: "roles" }
+      ],
+      loading: false
+    };
+  },
+
+  computed: {
+    ...mapGetters({
+      getList: "resource/getList"
+    }),
+
+    userList: function() {
+      return this.getList("user").map(item => {
+        return { key: item.id, value: item.name };
+      });
+    },
+
+    roleUsers: function() {
+      return this.getList("user").filter(
+        item =>
+          item.roles !== undefined
+            ? item.roles.find(role => role.id == this.id) != null
+            : false
+      );
+    }
+  },
+
+  mounted() {
+    this.loading = true;
+    let p1 = this.show({
+      resource: "role",
+      id: this.id,
+      query: { with: "users" }
+    }).then(data => {
+      this.currentRole = data;
+      this.form.name = this.currentRole.name;
+      this.form.users = this.currentRole.users.map(item => {
+        return { key: item.id, value: item.name };
+      });
+    });
+    let p2 = this.fetch({ resource: "user", query: { with: "roles" } });
+    Promise.all([p1, p2])
+      .then(data => {
+        this.loading = false;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+
+  methods: {
+    ...mapActions({
+      show: "resource/show",
+      fetch: "resource/list",
+      clear: "resource/show",
+      update: "resource/update"
+    }),
+
+    getRoles(item) {
+      if (item.roles.length !== 0)
+        return item.roles.reduce(
+          (a, c, i) => a + (i == 0 ? "" : ",") + c.name,
+          ""
+        );
+      else return "None";
+    },
+
+    onSave(e) {
+      e.target.disabled = true;
+      this.create({ resource: "role", data: this.form })
+        .then(respond => {
+          e.target.disabled = false;
+          console.log("role created successfuly");
+        })
+        .catch(error => {
+          console.log(error.response);
+          e.target.disabled = false;
+          this.$formFeedback(error.response.data.errors);
+        });
+      e.preventDefault();
+    },
+
+    onCancel(e) {
+      this.$router.go(-1);
+      e.preventDefault();
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
