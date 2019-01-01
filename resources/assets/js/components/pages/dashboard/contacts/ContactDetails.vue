f1d83654ae849fa166d1228c771e0af8f414d5c1
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-01-01 20:00:36 +0330

    modify contactDetails component

--- resources/assets/js/components/pages/dashboard/contacts/ContactDetails.vue
+++ resources/assets/js/components/pages/dashboard/contacts/ContactDetails.vue
@@ -4,38 +4,36 @@
     :loading="loading" 
     padding="p-5">
     <template slot="toolbar"/>
-        
+
     <template slot="drawer">
       <ul class="v-menu">
         <li class="menu-title">Actions</li>
-        <li><router-link 
-          class="btn btn-light" 
-          :to="{ name: 'contactCreate' }">New</router-link></li>
         <li>
-          <button 
+          <router-link 
             class="btn btn-light" 
-            @click="onSubmit">                        
-            Save
-          </button>
+            :to="{ name: 'contactCreate' }">New</router-link>
         </li>
         <li>
           <button 
             class="btn btn-light" 
-            @click="onCancel">                        
-            Cancel
-          </button>
+            @click="onSubmit">Save</button>
+        </li>
+        <li>
+          <button 
+            class="btn btn-light" 
+            @click="onCancel">Cancel</button>
         </li>
         <li class="menu-title">Links</li>
-        <li><router-link 
-          class="btn btn-link" 
-          :to="{ name: 'contactList' }">Contacts</router-link></li>
+        <li>
+          <router-link 
+            class="btn btn-link" 
+            :to="{ name: 'contactList' }">Contacts</router-link>
+        </li>
       </ul>
     </template>
-        
-    <template slot="content">
-            
-      <form>
 
+    <template slot="content">
+      <form>
         <div class="form-group">
           <div class="form-row">
             <label class="col-sm-2 col-form-lable">Group</label>
@@ -45,7 +43,7 @@
                 class="custom-select" 
                 id="group">
                 <option value="user">User</option>
-                <option value="customer">Customer</option>                                
+                <option value="customer">Customer</option>
               </select>
             </div>
           </div>
@@ -59,7 +57,7 @@
                 v-model="form.firstname" 
                 type="text" 
                 class="form-control" 
-                id="firstname" >
+                id="firstname">
             </div>
           </div>
         </div>
@@ -72,7 +70,7 @@
                 v-model="form.lastname" 
                 type="text" 
                 class="form-control" 
-                id="lastname" >
+                id="lastname">
             </div>
           </div>
         </div>
@@ -85,7 +83,7 @@
                 v-model="form.telephone" 
                 type="tel" 
                 class="form-control" 
-                id="telephone" >
+                id="telephone">
             </div>
           </div>
         </div>
@@ -98,7 +96,7 @@
                 v-model="form.mobilephone" 
                 type="tel" 
                 class="form-control" 
-                id="mobilephone" >
+                id="mobilephone">
             </div>
           </div>
         </div>
@@ -107,172 +105,55 @@
           <div class="form-row">
             <label class="col-sm-2 col-form-lable">Is a User</label>
             <div class="col-sm-10">
-
               <div class="custom-control custom-radio custom-control-inline">
-                <input 
-                  type="radio" 
-                  id="isUser" 
-                  name="isUser" 
-                  value="1" 
-                  class="custom-control-input" 
-                  checked>
+                <input
+                  type="radio"
+                  id="isUser"
+                  name="isUser"
+                  value="1"
+                  class="custom-control-input"
+                  checked
+                >
                 <label 
                   class="custom-control-label" 
                   for="isUser">Yes</label>
               </div>
               <div class="custom-control custom-radio custom-control-inline">
-                <input 
-                  type="radio" 
-                  id="notUser" 
-                  name="isUser" 
-                  value="0" 
-                  class="custom-control-input">
+                <input
+                  type="radio"
+                  id="notUser"
+                  name="isUser"
+                  value="0"
+                  class="custom-control-input"
+                >
                 <label 
                   class="custom-control-label" 
                   for="notUser">No</label>
-              </div>   
-
+              </div>
             </div>
           </div>
         </div>
+      </form>
 
-        <div class="form-group">
-          <div class="form-row">
-                         
-            <div class="col">
-              <label class="col-form-lable">Number</label>
-              <input 
-                v-model="address.number" 
-                type="text" 
-                class="form-control" 
-                id="mobilephone" >
-            </div>
-            <div class="col">
-              <label class="col-form-lable">Street</label>
-              <input 
-                v-model="address.street" 
-                type="text" 
-                class="form-control" 
-                id="mobilephone" >
-            </div>
-            <div class="col">
-              <label class="col-form-lable">Suburb</label>
-              <input 
-                v-model="address.suburb" 
-                type="text" 
-                class="form-control" 
-                id="mobilephone" >
-            </div>                         
-            <div class="col">
-              <label class="col-form-lable">Unit</label>
-              <input 
-                v-model="address.unit" 
-                type="text" 
-                class="form-control" 
-                id="mobilephone" >
-            </div>
-            <div class="col">
-              <label class="col-form-lable">Country</label>
-              <input 
-                v-model="address.country" 
-                type="text" 
-                class="form-control" 
-                id="mobilephone" >
-            </div>
-            <div class="col">
-              <label class="col-form-lable">Post Code</label>
-              <input 
-                v-model="address.postcode" 
-                type="text" 
-                class="form-control" 
-                id="mobilephone" >
-            </div>
-            <div class="col text-center">
-              <button 
-                type="button" 
-                class="btn btn-sm btn-light mt-4"
-                @click="onAddressAdd"
-                v-show="mode == 'add'">
-                <i class="material-icons">add</i>
-              </button>
-              <button 
-                type="button" 
-                class="btn btn-sm btn-light mt-4"
-                @click="onAddressSave"
-                v-show="mode == 'edit'">
-                <i class="material-icons">save</i>
-              </button>  
-              <button 
-                type="button" 
-                class="btn btn-sm btn-light mt-4"
-                @click="onAddressCancel"
-                v-show="mode == 'edit'">
-                <i class="material-icons">clear</i>
-              </button>                                                       
-            </div>
-          </div>
-        </div>
-                
-      </form>    
-
-      <div class="table-responsive">  
-        <table 
-          v-show="form.addresses.length != 0" 
-          class="table table-sm table-hover table-light">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Number</th>
-              <th scope="col">Street</th>
-              <th scope="col">Subrb</th>
-              <th scope="col">Unit</th>
-              <th scope="col">Country</th>
-              <th scope="col">Post Code</th>
-              <th scope="col"/>
-            </tr>
-          </thead>
-          <tbody>
-            <tr 
-              v-for="(item, index) in form.addresses" 
-              :key="index">
-              <th scope="row">{{ index + 1 }}</th>                        
-              <td >
-                <div :id="'addresses-' + index + '-number'">{{ item.number }}</div>                            
-              </td>
-              <td>
-                <div :id="'addresses-' + index + '-street'">{{ item.street }}</div>
-              </td>
-              <td>
-                <div :id="'addresses-' + index + '-suburb'">{{ item.suburb }}</div>
-              </td>
-              <td>
-                <div :id="'addresses-' + index + '-unit'">{{ item.unit }}</div>
-              </td>
-              <td>
-                <div :id="'addresses-' + index + '-country'">{{ item.country }}</div>
-              </td>
-              <td>
-                <div :id="'addresses-' + index + '-postcode'">{{ item.postcode }}</div>
-              </td>
-              <td>
-                <button 
-                  type="button" 
-                  class="btn btn-sm btn-light" 
-                  @click="onAddressEdit(item, index)">
-                  <i class="material-icons">edit</i>
-                </button>
-                <button 
-                  type="button" 
-                  class="btn btn-sm btn-light" 
-                  @click="onAddressDelete(item, index)">
-                  <i class="material-icons">remove</i>
-                </button>   
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>  
-                
+      <div class="table-responsive">
+        <ts-grid
+          v-model="form.addresses"
+          :columns="columns"
+          :has-toolbar="false"
+          @insert="addAddress"
+          @edit="editAddress"
+          @select="(selects)=>{ this.selects = selects }"
+        >
+          <template slot-scope="{ item }">
+            <td>{{ item.number }}</td>
+            <td>{{ item.street }}</td>
+            <td>{{ item.suburb }}</td>
+            <td>{{ item.unit }}</td>
+            <td>{{ item.country }}</td>
+            <td>{{ item.postcode }}</td>
+          </template>
+        </ts-grid>
+      </div>
     </template>
   </nav-view>
 </template>
@@ -293,8 +174,6 @@ export default {
   data() {
     return {
       loading: false,
-      mode: "add",
-      index: 0,
       form: {
         group: "",
         firstname: "",
@@ -303,15 +182,21 @@ export default {
         mobilephone: "",
         addresses: []
       },
-      address: {
-        number: "",
-        street: "",
-        suburb: "",
-        unit: "",
-        country: "",
-        postcode: ""
-      }
+      columns: [
+        { key: 1, value: "Number" },
+        { key: 2, value: "Street" },
+        { key: 3, value: "Suburb" },
+        { key: 4, value: "Unit" },
+        { key: 5, value: "Country" },
+        { key: 6, value: "Post Code" }
+      ]
     };
+  },
+
+  watch: {
+    selects: function(val) {
+      console.log(val);
+    }
   },
 
   mounted() {
@@ -337,42 +222,22 @@ export default {
       update: "resource/update"
     }),
 
-    onAddressAdd() {
-      this.form.addresses.push(Object.assign({}, this.address));
-      this.clearAddress();
-    },
-
-    onAddressEdit(item, index) {
-      this.clearAddress();
-      this.mode = "edit";
-      this.index = index;
-      Object.assign(this.address, this.form.addresses[index]);
-    },
-
-    onAddressDelete(item, index) {
-      this.clearAddress();
-      this.mode = "add";
-      this.form.addresses.splice(index, 1);
-    },
-
-    onAddressCancel() {
-      this.mode = "add";
-      this.clearAddress();
-    },
-
-    onAddressSave() {
-      Object.assign(this.form.addresses[this.index], this.address);
-    },
-
-    clearAddress() {
-      this.address = {};
+    addAddress() {
+      this.form.addresses.push({
+        number: "",
+        street: "",
+        suburb: "",
+        unit: "",
+        country: "",
+        postcode: ""
+      });
     },
 
     onSubmit() {
       this.update({ resource: "contact", id: this.id, data: this.form })
         .then(() => {
           console.log("Contact updated successfuly.");
-          this.$router.push({name:'contactList'});          
+          this.$router.push({ name: "contactList" });
         })
         .catch(error => {
           console.log(error.response);
@@ -381,7 +246,7 @@ export default {
     },
 
     onCancel() {
-      this.$router.push({name:'contactList'});
+      this.$router.push({ name: "contactList" });
     }
   }
 };
