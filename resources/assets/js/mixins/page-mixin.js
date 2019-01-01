448dcf6e155f3ffa2d8830206897f52fb58caa49
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-01-01 11:32:07 +0330

    add new mixin

---
+++ resources/assets/js/mixins/page-mixin.js
@@ -0,0 +1,53 @@
+import { mapActions } from 'vuex';
+
+const SNACKBAR_THEME = {
+    primary: 'alert alert-primary',
+    secondary: 'alert alert-secondary',
+    success: 'alert alert-success',
+    danger: 'alert alert-danger',
+    warning: 'alert alert-warning',
+    info: 'alert alert-info',
+    light: 'alert alert-light',
+    dark: 'alert alert-dark',
+}
+
+export default {
+    data() {
+        return {
+            isLoading: false
+        }
+    },
+
+    methods: {
+        ...mapActions({
+            snackbar: 'core/snackbar'
+        }),
+
+        showMessage(message, theme = 'primary', timeout = 3000, fixed = false) {
+            this.snackbar({
+                show: true,
+                message: message,
+                theme: SNACKBAR_THEME[theme],
+                timeout: timeout,
+                fixed: fixed
+            });
+        },
+
+        showError(error, message = '') {
+            this.snackbar({
+                show: true,
+                message: '',
+                theme: SNACKBAR_THEME.danger,
+                fixed: true
+            });
+        },
+
+        loadingStart() {
+            this.isLoading = true;
+        },
+
+        loadingStop() {
+            this.isLoading = false;
+        }
+    }
+}
