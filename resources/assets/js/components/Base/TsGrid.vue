d49f5643553269ec3f1449f43c60a5f46b4ac587
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-01-01 19:59:52 +0330

    add grid and tablebase components

---
+++ resources/assets/js/components/Base/TsGrid.vue
@@ -0,0 +1,66 @@
+<template>
+  <base-table 
+    :value="value"
+    @input="(data)=>{ $emit('input', data) }"
+    @select="(selects)=>{ $emit('select', selects) }">
+
+    <table
+      class="table table-sm table-hover table-light" 
+      slot-scope="{ toggleRow, isSelected, deleteRow }">
+     
+      <thead> 
+        <tr v-if="hasToolbar"> 
+          <th :colspan="columns.length + 1">
+            <slot name="toolbar" />       
+          </th>    
+        </tr>
+        <tr>
+          <th>
+            <button 
+              type="button"
+              @click="$emit('insert')">ADD NEW</button>   
+          </th>
+          <th 
+            v-for="column in columns" 
+            :key="column.key">
+            {{ column.value }}
+          </th>   
+        </tr>        
+      </thead>
+      <tbody>        
+        <tr 
+          v-for="row in value" 
+          :key="row.id">
+          <td>
+            <button 
+              type="button"
+              @click="$emit('edit', row)">Edite</button>
+            <button 
+              type="button"
+              @click="deleteRow(row)">Delete</button>
+          </td>
+          <slot :item="row" />
+        </tr>
+      </tbody>
+    </table>
+
+  </base-table>
+</template>
+
+<script>
+import BaseTable from "./core/BaseTable.vue";
+export default {
+  name: "TsGrid",
+
+  components: {
+    "base-table": BaseTable
+  },
+
+  props: ["value", "columns", 'hasToolbar'],
+
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
