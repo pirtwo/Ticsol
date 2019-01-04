a2e67050fd03deeac0c5d657d689877f7c7b9ad0
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-01-04 20:06:01 +0330

    create component for google places

---
+++ resources/assets/js/components/Base/GooglePlaces.vue
@@ -0,0 +1,55 @@
+<template>  
+  <input 
+    ref="autoComplete" 
+    class="form-control" 
+    type="text">  
+</template>
+
+<script>
+export default {
+  name: "GooglePlaces",
+
+  props: {
+    value: {
+      type: Object,
+      default: () => {
+        return {};
+      }
+    },
+    options: {
+      type: Object,
+      default: () => {
+        return { types: ["regions"] };
+      }
+    }
+  },
+
+  data() {
+    return {
+      autoComplete: {}
+    };
+  },
+
+  created() {
+    
+  },
+
+  mounted() {
+    this.autoComplete = new google.maps.places.Autocomplete(
+      this.$refs.autoComplete,
+      this.options
+    );
+    this.autoComplete.addListener('place_changed', this.onPlaceChange);
+  },
+
+  methods: {
+    onPlaceChange(){
+      console.log(this.autoComplete.getPlace());
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
