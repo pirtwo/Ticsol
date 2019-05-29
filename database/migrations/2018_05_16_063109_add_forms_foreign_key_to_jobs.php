6b9ff578e9a64e293bf32be1a121f6c4798ae458
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-05-29 19:29:59 +0430

    update migration for cascade delete

--- database/migrations/2018_05_16_063109_add_forms_foreign_key_to_jobs.php
+++ database/migrations/2018_05_16_063109_add_forms_foreign_key_to_jobs.php
@@ -16,7 +16,8 @@ class AddFormsForeignKeyToJobs extends Migration
         Schema::table('ts_jobs', function (Blueprint $table) {
             $table->foreign('form_id')
                 ->references('id')
-                ->on('ts_forms');
+                ->on('ts_forms')
+                ->onDelete('cascade');
         });
     }
 
