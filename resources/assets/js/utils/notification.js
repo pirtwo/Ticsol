1ba2d6a0db59752e374eff4ccbf31661e9b9474d
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-06-18 13:47:11 +0430

    add some files

---
+++ resources/assets/js/utils/notification.js
@@ -0,0 +1,23 @@
+import uuid from './uuid';
+
+export default class Notification {
+  constructor({
+    type = 'info',
+    title,
+    message,
+    autoHide = true,
+    delay = 3000,
+    seen = false,
+    date = Date.now()
+  }) {
+    this.id = uuid();
+    this.type = type;
+    this.title = title;
+    this.message = message;
+    this.autoHide = autoHide;
+    this.delay = delay;
+    this.seen = seen;
+    this.date = date;
+  }
+}
+
