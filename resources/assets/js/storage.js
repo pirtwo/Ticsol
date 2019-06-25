e53733c89a19674505ae653c982a6867f76985d5
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-06-25 19:41:54 +0430

    filtered some states from storeing

--- resources/assets/js/storage.js
+++ resources/assets/js/storage.js
@@ -1,7 +1,14 @@
 import VuexPersist from 'vuex-persist';
 
 export const localStorage = new VuexPersist({
-    key: 'Vuex',
-    storage: window.localStorage,
-    modules: ['user', 'core']
-})
+  key: 'app.ticsol',
+  storage: window.localStorage,
+  reducer: state => ({
+    user: state.user,
+    core: {
+      theme: state.core.theme,
+      drawer: state.core.drawer,
+      notifications: state.core.notifications
+    }
+  })
+});
