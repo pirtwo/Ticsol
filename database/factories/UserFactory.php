2162247c8c6d5a318b265624229ae073558b215b
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-06-26 11:59:56 +0430

    modify user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,12 +6,14 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     static $password;
     $index = $faker->numberBetween(1, 8);
+    $firstname = $faker->firstname;
+    $lastname = $faker->lastname;
 
     return [
         'client_id' => $faker->numberBetween(1, 3),
-        'name' => $faker->name,
-        'firstname' => $faker->firstname,
-        'lastname' => $faker->lastname,
+        'name' => $firstname . " " . $lastname,
+        'firstname' => $firstname,
+        'lastname' => $lastname,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),              
         'meta' => [ 'avatar' => str_replace_array('?', [$index], '/img/avatar/pic_?.jpg') ]
