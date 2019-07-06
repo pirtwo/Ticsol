a7842898aec66a5f1b37aa885c0c65aa45378680
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-07-06 21:30:57 +0430

    fixed some issues

--- resources/assets/js/components/pages/dashboard/jobs/JobModify.vue
+++ resources/assets/js/components/pages/dashboard/jobs/JobModify.vue
@@ -54,7 +54,7 @@
             Jobs
           </router-link>
         </li>
-        <li v-if="currentJob">
+        <!-- <li v-if="job">
           <router-link
             class="btn btn-link"
             :to="{ name: 'jobList', params : { col: 'job_id', opt: 'eq', val: this.id } }"
@@ -62,67 +62,68 @@
             Schedule
             Items
           </router-link>
-        </li>
-        <li v-if="currentJob">
-          <router-link
+        </li> -->
+        <li v-if="job">
+          <router-link   
+            role="button"       
             class="btn btn-link"
             :to="{ name: 'commentList', params : { entity: 'job', id: this.id } }"
           >
             Comments
             <ts-badge class="badge-light ml-auto">
-              {{ currentJob.commentsCount }}
+              {{ job.commentsCount }}
             </ts-badge>
           </router-link>
         </li>
-        <li v-if="currentJob">
+        <li v-if="job">
           <router-link
-            :class="[{'disabled' : this.relatedActivities === null}, 'btn btn-link' ]"
+            :class="[{'disabled' : this.job.reportsCount === 0}, 'btn btn-link' ]"
             role="button"
-            :aria-disabled="this.relatedActivities === null"
-            :to="{ name: 'activityList', params : { col: 'id', opt: 'in', val: this.relatedActivities } }"
+            :aria-disabled="this.job.reportsCount === 0"
+            :to="{ name: 'activityList', params : { opt: 'eq', col: 'job_id', val: this.id } }"
           >
             Activity Reports
             <ts-badge class="badge-light ml-auto">
-              {{ currentJob.reportsCount }}
+              {{ job.reportsCount }}
             </ts-badge>
           </router-link>
         </li>
-        <li v-if="currentJob">
+        <li v-if="job">
           <router-link
-            :class="[{'disabled' : this.relatedJobs === null}, 'btn btn-link' ]"
+            :class="[{'disabled' : this.job.childsCount === 0}, 'btn btn-link' ]"
             role="button"
-            :aria-disabled="this.relatedJobs === null"
-            :to="{ name: 'jobList', params : { col: 'id', opt: 'in', val: this.relatedJobs } }"
+            :aria-disabled="this.job.childsCount === 0"
+            :to="{ name: 'jobList', params : { opt: 'eq', col: 'parent_id', val: this.id } }"
           >
             Related Jobs
             <ts-badge class="badge-light ml-auto">
-              {{ currentJob.childsCount }}
+              {{ job.childsCount }}
             </ts-badge>
           </router-link>
         </li>
-        <li v-if="currentJob">
+        <li v-if="job">
           <router-link
-            :class="[{'disabled' : this.relatedRequests === null}, 'btn btn-link' ]"
+            :class="[{'disabled' : this.job.requestsCount === 0 }, 'btn btn-link' ]"
             role="button"
-            :aria-disabled="this.relatedRequests === null"
-            :to="{ name: 'inbox', params : { col: 'id', opt: 'in', val: this.relatedRequests } }"
+            :aria-disabled="this.job.requestsCount === 0"
+            :to="{ name: 'inbox', params : { opt: 'eq', col: 'job_id', val: this.id } }"
           >
             Related Requests
             <ts-badge class="badge-light ml-auto">
-              {{ currentJob.requestsCount }}
+              {{ job.requestsCount }}
             </ts-badge>
           </router-link>
         </li>
-        <li v-if="currentJob">
+        <li v-if="job">
           <router-link
-            :class="[{'disabled' : this.relatedContacts === null}, 'btn btn-link' ]"
+            :class="[{'disabled' : this.job.contactsCount === 0}, 'btn btn-link' ]"
             role="button"
-            :aria-disabled="this.relatedContacts === null"
-            :to="{ name: 'contactList', params : { col: 'id', opt: 'in', val: this.relatedContacts } }"
+            :aria-disabled="this.job.contactsCount === 0"
+            :to="{ name: 'contactList', params : { opt: 'in', col: 'contact.jobs.id', val: this.id } }"
           >
             Related Contacts
             <ts-badge class="badge-light ml-auto">
-              {{ currentJob.contactsCount }}
+              {{ job.contactsCount }}
             </ts-badge>
           </router-link>
         </li>
@@ -272,6 +273,7 @@ import { mapGetters, mapActions } from "vuex";
 import { required } from "vuelidate/lib/validators";
 import PageMixin from "../../../../mixins/page-mixin.js";
 import FormGen from "../../../base/formGenerator/BaseFormGenerator";
+import { Promise } from 'q';
 
 export default {
   name: "JobModify",
@@ -286,7 +288,7 @@ export default {
 
   data() {
     return {
-      currentJob: null,
+      jobList: [],
       schema: "",
       formData: "",
       form: {
@@ -309,6 +311,10 @@ export default {
   },
 
   watch: {
+    job:function(val){
+      this.populateForm(val);
+    },
+
     "form.profile": function(value) {
       if (value.key) {
         this.schema = this.getList(
@@ -317,15 +323,20 @@ export default {
         )[0].schema;
       }
     }
-  },
+  }, 
 
   computed: {
     ...mapGetters({
       getList: "resource/getList"
     }),
 
+    job:function(){
+      if(!this.id) return null;
+      return this.getList("job")[0];
+    },
+
     jobs: function() {
-      return this.getList("job").map(item => {
+      return this.jobList.map(item => {
         return { key: item.id, value: item.title };
       });
     },
@@ -340,116 +351,69 @@ export default {
       return this.getList("contact").map(item => {
         return { key: item.id, value: `${item.firstname} ${item.lastname}` };
       });
-    },
-
-    relatedContacts: function() {
-      if (this.currentJob != null && this.currentJob.contacts.length > 0)
-        return this.currentJob.contacts.reduce(
-          (a, c, i) => a + (i == 0 ? "" : ",") + c.id,
-          ""
-        );
-      else return null;
-    },
-
-    relatedJobs: function() {
-      if (this.currentJob != null && this.currentJob.childs.length > 0)
-        return this.currentJob.childs.reduce(
-          (a, c, i) => a + (i == 0 ? "" : ",") + c.id,
-          ""
-        );
-      else return null;
-    },
-
-    relatedActivities: function() {
-      if (this.currentJob != null && this.currentJob.activities.length > 0)
-        return this.currentJob.activities.reduce(
-          (a, c, i) => a + (i == 0 ? "" : ",") + c.id,
-          ""
-        );
-      else return null;
-    },
-
-    relatedRequests: function() {
-      if (this.currentJob != null && this.currentJob.requests.length > 0) {
-        return this.currentJob.requests.reduce(
-          (a, c, i) => a + (i == 0 ? "" : ",") + c.id,
-          ""
-        );
-      } else return null;
-    }
+    }    
   },
 
   beforeRouteEnter(to, from, next) {    
     next(vm => {
       vm.clearForm();
+      vm.loadAssets();
     });
   },
 
-  mounted() {
-    this.startLoading();
-    if (this.id) {
-      let p1 = this.fetch({
-        resource: "job",
-        query: { with: "childs,contacts,requests,activities" }
-      });
-      let p2 = this.fetch({ resource: "form" });
-      let p3 = this.fetch({ resource: "contact" });
-      Promise.all([p1, p2, p3])
-        .then(() => {
-          this.currentJob = this.getList("job", item => item.id == this.id)[0];
-          this.form.title = this.currentJob.title;
-          this.form.code = this.currentJob.code;
-
-          this.form.parent =
-            this.currentJob.parent_id !== null
-              ? this.jobs.find(item => item.key == this.currentJob.parent_id)
-              : {};
-
-          this.form.profile =
-            this.currentJob.form_id !== null
-              ? this.profiles.find(item => item.key == this.currentJob.form_id)
-              : {};
-
-          this.form.contacts = this.contacts.filter(item =>
-            this.currentJob.contacts.find(elm => elm.id === item.key)
-          );
-
-          this.form.meta = this.currentJob.meta;
-
-          this.schema =
-            this.currentJob.form_id !== null
-              ? this.getList(
-                  "form",
-                  item => item.id == this.currentJob.form_id
-                )[0].schema
-              : null;
-
-          this.stopLoading();
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    } else {
-      let p1 = this.fetch({ resource: "job" });
-      let p2 = this.fetch({ resource: "form" });
-      let p3 = this.fetch({ resource: "contact" });
-      Promise.all([p1, p2, p3])
-        .then(() => {
-          this.stopLoading();
-        })
-        .catch(error => {
-          this.stopLoading();
-          this.showMessage(error.message, "danger");
-        });
-    }
-  },
+  beforeRouteLeave (to, from, next) {
+    this.clear('job');
+    this.clear('contact');
+    this.clear('form');   
+    next(); 
+  },  
 
   methods: {
     ...mapActions({
-      fetch: "resource/list",
+      clear: 'resource/clearResource',
+      list: "resource/list",
       create: "resource/create",
       update: "resource/update"
     }),
+
+    async loadAssets(){
+      this.startLoading();
+
+      let p1 = this.list({resource:'form'});      
+      let p2 = this.list({resource:'contact'});
+      let p3 = await this.list({resource:'job'})
+        .then((data) =>{ this.jobList = data; });
+
+      let p4 = await this.id ? 
+        this.list({resource:'job', query: { 
+          eq: `id,${this.id}`, 
+          with: "parent,profile,contacts" 
+        }}) : new Promise((resolve)=>resolve());
+
+      Promise.all([p1,p2,p3,p4]).finally(()=>{
+        this.stopLoading();
+      });
+    },
+
+    populateForm(job){
+      this.form.title = job.title;
+      this.form.code = job.code;
+      this.form.parent = job.parent ? { key: job.parent.id, value: job.parent.title } : {};
+      this.form.profile = job.profile ? { key: job.profile.id, value: job.profile.name } : {};
+      this.form.contacts = job.contacts ? job.contacts.map(item=>{
+        return { key: item.id, value: `${item.firstname} ${item.lastname}` };
+      }) : [];
+
+      this.form.meta = job.meta;
+
+      this.schema =
+        job.form_id !== null
+          ? this.getList(
+              "form",
+              item => item.id == job.form_id
+            )[0].schema
+          : null;
+    },
 
     onSubmit(e) {
       e.preventDefault();
