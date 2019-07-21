aa6fef618f5a6d3beeecf197cc54a5bd5b73663d
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-07-21 19:28:07 +0430

    adding web auth routes

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,34 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::group([ 'prefix' => '', 'namespace' => 'App\Ticsol\Base\Controllers'], function(){
-    Route::get('/{any}', 'AppController@index')->where('any', '.*')->name('home');
+/**
+ * web auth routes.
+ */
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    // Authentication Routes
+    Route::get('login', 'Auth\LoginController@showLoginForm')
+        ->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout')
+        ->name('logout');
+
+    // Password Reset Routes
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')
+        ->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')
+        ->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')
+        ->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 });
 
-
-
+/**
+ * web app routes.
+ */
+Route::group(['prefix' => '', 'namespace' => 'App\Ticsol\Base\Controllers'], function () {
+    Route::get('/{any}', 'AppController@index')
+        ->where('any', '.*')
+        ->name('home');
+});
