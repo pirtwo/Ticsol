e45a0791416577f80b8d1521a396f8bad13e8d0e
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-07-25 20:16:16 +0430

    modify methods and add webhook routes

--- routes/api.php
+++ routes/api.php
@@ -19,77 +19,84 @@ Route::group(['prefix' => '', 'namespace' => 'App\Ticsol\Components\Controllers\
      */
     Route::middleware('auth:api')->group(function () {
         // User
-        Route::get('user', 'UserController@index');
-        Route::get('user/info', 'UserController@current');
+        Route::get('me', 'UserController@me');
+        Route::get('user', 'UserController@index');        
         Route::get('user/show/{id}', 'UserController@show');
-        Route::post('user/update/{id}', 'UserController@update');
+        Route::put('user/update/{id}', 'UserController@update');
 
         // Role
         Route::get('role', 'RoleController@index');
         Route::get('role/show/{id}', 'RoleController@show');
         Route::post('role/store', 'RoleController@store');
-        Route::post('role/update/{id}', 'RoleController@update');
-        Route::post('role/delete/{id}', 'RoleController@delete');
+        Route::put('role/update/{id}', 'RoleController@update');
+        Route::delete('role/delete/{id}', 'RoleController@delete');
 
         // Job
         Route::get('job', 'JobController@index');
         Route::get('job/show/{id}', 'JobController@show');
         Route::post('job/store', 'JobController@store');
-        Route::post('job/update/{id}', 'JobController@update');
+        Route::put('job/update/{id}', 'JobController@update');
 
         // Schedule
         Route::get('schedule', 'ScheduleController@index');
         Route::get('schedule/show/{id}', 'ScheduleController@show');
         Route::post('schedule/store', 'ScheduleController@store');
-        Route::post('schedule/update/{id}', 'ScheduleController@update');
-        Route::post('schedule/delete/{id}', 'ScheduleController@delete');
+        Route::put('schedule/update/{id}', 'ScheduleController@update');
+        Route::delete('schedule/delete/{id}', 'ScheduleController@delete');
 
         // Timesheet
         Route::get('timesheet', 'TimesheetController@index');
         Route::get('timesheet/show/{id}', 'TimesheetController@show');
         Route::post('timesheet/store', 'TimesheetController@store');
-        Route::post('timesheet/update/{id}', 'TimesheetController@update');
-        Route::post('timesheet/delete/{id}', 'TimesheetController@delete');
+        Route::put('timesheet/update/{id}', 'TimesheetController@update');
+        Route::delete('timesheet/delete/{id}', 'TimesheetController@delete');
 
         // Team
         Route::get('team', 'TeamController@index');
         Route::get('team/show/{id}', 'TeamController@show');
         Route::post('team/store', 'TeamController@store');
-        Route::post('team/update/{id}', 'TeamController@update');
-        Route::post('team/delete/{id}', 'TeamController@delete');
+        Route::put('team/update/{id}', 'TeamController@update');
+        Route::delete('team/delete/{id}', 'TeamController@delete');
 
         // Activity
         Route::get('activity', 'ActivityController@index');
         Route::get('activity/show/{id}', 'ActivityController@show');
         Route::post('activity/store', 'ActivityController@store');
-        Route::post('activity/update/{id}', 'ActivityController@update');
+        Route::put('activity/update/{id}', 'ActivityController@update');
 
         // Form
         Route::get('form', 'FormController@index');
         Route::get('form/show/{id}', 'FormController@show');
         Route::post('form/store', 'FormController@store');
-        Route::post('form/update/{id}', 'FormController@update');
+        Route::put('form/update/{id}', 'FormController@update');
+        Route::delete('form/delete/{id}', 'FormController@delete');
 
         // Contact
         Route::get('contact', 'ContactController@index');
         Route::get('contact/show/{id}', 'ContactController@show');
         Route::post('contact/store', 'ContactController@store');
-        Route::post('contact/update/{id}', 'ContactController@update');
+        Route::put('contact/update/{id}', 'ContactController@update');
+        Route::delete('contact/delete/{id}', 'ContactController@delete');
 
         // Request
         Route::get('request', 'RequestController@index');
         Route::get('request/show/{id}', 'RequestController@show');
         Route::post('request/store', 'RequestController@store');
-        Route::post('request/update/{id}', 'RequestController@update');
-        Route::post('request/approve/{id}', 'RequestController@approve');
-        Route::post('request/reject/{id}', 'RequestController@reject');
+        Route::put('request/update/{id}', 'RequestController@update');
+        Route::put('request/approve/{id}', 'RequestController@approve');
+        Route::put('request/reject/{id}', 'RequestController@reject');
         Route::get('request/attachment/{reqId}/{fileId}', 'RequestController@attachment');
 
         // Comment
         Route::get('comment', 'CommentController@index');
         Route::post('comment/store', 'CommentController@store');
-        Route::post('comment/update/{id}', 'CommentController@update');
-        Route::post('comment/delete/{id}', 'CommentController@delete');
+        Route::put('comment/update/{id}', 'CommentController@update');
+        Route::delete('comment/delete/{id}', 'CommentController@delete');
+
+        // webhooks
+        Route::post('hooks', 'WebhookController@subscribe');
+        Route::delete('hooks/{id}', 'WebhookController@delete');
+        Route::get('polling/trigger', 'WebhookController@pollForTrigger');
     });
 
 });
