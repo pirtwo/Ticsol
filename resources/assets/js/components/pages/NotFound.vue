7c4eea52ba20ee1d2db1de0a3c92ad807bf89e67
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-08-11 09:55:36 +0430

    add not found  page

---
+++ resources/assets/js/components/pages/NotFound.vue
@@ -0,0 +1,48 @@
+<template>
+  <app-layout 
+    :header="false" 
+    :footer="false"
+  >
+    <div class="wrap-content d-flex justify-content-center align-items-center h-100">      
+      <div class="md-layout md-alignment-center-center">
+        <div class="wrap-auth md-layout-item">
+          <img 
+            src="/img/logo.svg" 
+            class="app-logo"
+          >
+          <div class="text-center">
+            <h3>Whoops, page not found</h3>
+            <p class="text-muted">
+              We didn't find the page. Go to  
+              <router-link to="/home">
+                home
+              </router-link> or
+              <button
+                class="btn btn-link"
+                type="button"
+                @click="goBack"
+              >
+                back
+              </button>
+            </p>
+          </div> 
+        </div>     
+      </div>
+    </div>
+  </app-layout>  
+</template>
+<script>
+export default {
+  name: "NotFound",
+
+  methods:{
+    goBack(){
+      console.log("1");
+      this.$router.go(-1);
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
