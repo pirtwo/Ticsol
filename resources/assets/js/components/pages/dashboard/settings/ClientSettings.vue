f34c2bb85de5d5c951222a451c3643c5080cba3d
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-08-11 09:56:30 +0430

    add user and client settings

---
+++ resources/assets/js/components/pages/dashboard/settings/ClientSettings.vue
@@ -0,0 +1,322 @@
+<template>
+  <app-main
+    :scrollbar="true"
+    :loading="isLoading"
+    padding="p-5"
+  >
+    <template slot="drawer">
+      <ul class="v-menu">
+        <li class="menu-title">
+          Actions
+        </li>
+
+        <li>
+          <button
+            type="button"
+            class="btn"
+            @click="onSave"
+          >
+            Save
+          </button>
+        </li>
+
+        <li>
+          <button
+            type="button"
+            class="btn"
+            @click="onCancel"
+          >
+            Cancel
+          </button>
+        </li>
+
+        <li class="menu-title">
+          Links
+        </li>
+      </ul>
+    </template>
+    <template slot="content">
+      <!-- user settings -->
+      <form>
+        <!-- business hours -->
+        <div class="form-group">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Business Hours</label>
+            <div class="col-sm-10">
+              <div
+                v-for="(item, index) in form.businessHours"
+                :key="item.day"
+                class="form-row mt-1"
+              >
+                <!-- day label -->
+                <div class="col-md-2">
+                  {{ getDayName(item.day) }}
+                </div>
+
+                <!-- isopen -->
+                <div class="col-md-2">
+                  <div class="custom-control custom-checkbox">
+                    <input
+                      v-model="item.isopen"
+                      type="checkbox"
+                      :id="`isopen-${index}`"
+                      class="custom-control-input"
+                    >
+                    <label
+                      :for="`isopen-${index}`"
+                      class="custom-control-label"
+                    >Open</label>
+                  </div>
+                </div>
+
+                <!-- start -->
+                <div class="col mr-md-2">
+                  <div class="form-row">
+                    <label class="col-sm-2">From</label>
+                    <input
+                      v-model="item.start"
+                      type="time"
+                      class="form-control col"
+                      :disabled="!item.isopen"
+                    >
+                  </div>                  
+                </div>
+
+                <!-- end -->
+                <div class="col ml-md-2">
+                  <div class="form-row">
+                    <label class="col-sm-2">Till</label>
+                    <input
+                      v-model="item.end"
+                      type="time"
+                      class="form-control col"
+                      :disabled="!item.isopen"
+                    >
+                  </div>                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- schedule view -->
+        <div class="form-group mt-4">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Schedule View</label>
+            <div class="col-sm-10">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleView"
+                  type="radio"
+                  id="view1"
+                  name="views"
+                  value="employee"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="view1"
+                >Employee</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleView"
+                  type="radio"
+                  id="view2"
+                  name="views"
+                  value="job"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="view2"
+                >Jobs</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- schedule range -->
+        <div class="form-group">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Schedule Range</label>
+            <div class="col-sm-10">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleRange"
+                  type="radio"
+                  id="range1"
+                  name="ranges"
+                  value="week"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="range1"
+                >Week</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleRange"
+                  type="radio"
+                  id="range2"
+                  name="ranges"
+                  value="month"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="range2"
+                >Month</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- hour per day -->
+        <div class="form-group mt-4">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Hour Per Day</label>
+            <div class="col-sm-10">
+              <input
+                v-model="form.hourPerDay"
+                type="text"
+                class="form-control"
+              >
+            </div>
+          </div>
+        </div>
+      </form>
+    </template>
+  </app-main>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import pageMixin from "../../../../mixins/page-mixin";
+
+export default {
+  name: "ClientSettings",
+
+  mixins: [pageMixin],
+
+  data() {
+    return {
+      form: {
+        businessHours: [
+          { day: 1, start: "00:00", end: "00:00", isopen: false },
+          { day: 2, start: "00:00", end: "00:00", isopen: false },
+          { day: 3, start: "00:00", end: "00:00", isopen: false },
+          { day: 4, start: "00:00", end: "00:00", isopen: false },
+          { day: 5, start: "00:00", end: "00:00", isopen: false },
+          { day: 6, start: "00:00", end: "00:00", isopen: false },
+          { day: 0, start: "00:00", end: "00:00", isopen: false }
+        ],
+        scheduleView: "",
+        scheduleRange: "",
+        hourPerDay: 0
+      }
+    };
+  },
+
+  computed: {
+    ...mapGetters({
+      clientId: "user/getClientId"
+    })
+  },
+
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.loadAssets();
+    });
+  },
+
+  beforeRouteLeave(to, from, next) {
+    this.clear("client");
+    next();
+  },
+
+  methods: {
+    ...mapActions({
+      show: "resource/show",
+      update: "resource/update",
+      clear: "resource/clearResource"
+    }),
+
+    loadAssets() {
+      this.startLoading();
+      this.show({ resource: "client", id: this.clientId }).then(
+        data => {
+          this.populateForm(data.meta);
+          this.stopLoading();
+        }
+      );
+    },
+
+    populateForm(settings) {
+      if(settings.business_hours){
+        for (let i = 0; i < settings.business_hours.length; i++) {
+          this.form.businessHours[i] = Object.assign({}, settings.business_hours[i]);    
+        }
+      }
+
+      this.form.scheduleView = settings.schedule_view;
+      this.form.scheduleRange = settings.schedule_range;
+      this.form.hourPerDay = settings.hour_per_day;
+    },
+
+    getDayName(dayNum) {
+      switch (dayNum) {
+        case 0:
+          return "Sunday";
+        case 1:
+          return "Monday";
+        case 2:
+          return "Tuesday";
+        case 3:
+          return "Wednesday";
+        case 4:
+          return "Thursday";
+        case 5:
+          return "Friday";
+        case 6:
+          return "Saturday";
+        default:
+          return "";
+      }
+    },
+
+    onSave(e) {
+      e.preventDefault();
+      e.target.disable = true;
+
+      // setup the req body
+      let form = {};     
+      form.business_hours = this.form.businessHours; 
+      form.schedule_view = this.form.scheduleView;
+      form.schedule_range = this.form.scheduleRange;
+      form.hour_per_day = this.form.hourPerDay;
+
+      this.update({ resource: "client", id: this.clientId, data: form })
+        .then(data => {
+          this.populateForm(data.meta);
+          this.showMessage(`Settings updated successfuly.`, "success");
+        })
+        .catch(error => {
+          this.showMessage(error.message, "danger");
+        })
+        .finally(() => {
+          e.target.disable = false;
+        });
+    },
+
+    onCancel(e) {
+      e.preventDefault();
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ resources/assets/js/components/pages/dashboard/settings/UserSettings.vue
@@ -0,0 +1,315 @@
+<template>
+  <app-main
+    :scrollbar="false"
+    :loading="isLoading"
+    padding="p-5"
+  >
+    <template slot="drawer">
+      <ul class="v-menu">
+        <li class="menu-title">
+          Actions
+        </li>
+
+        <li>
+          <button
+            type="button"
+            class="btn"
+            @click="onSave"
+          >
+            Save
+          </button>
+        </li>
+
+        <li>
+          <button
+            type="button"
+            class="btn"
+            @click="onCancel"
+          >
+            Cancel
+          </button>
+        </li>
+
+        <li class="menu-title">
+          Links
+        </li>
+      </ul>
+    </template>
+    <template slot="content">
+      <!-- user settings -->
+      <form>
+        <!-- theme -->
+        <div class="form-group">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Theme</label>
+            <div class="col-sm-10">
+              <ts-select
+                v-model="form.theme"
+                :data="themes"
+                id="parent_id"
+                name="jobParent"
+                placeholder="select parent..."
+                search-placeholder="search..."
+              />
+            </div>
+          </div>
+        </div>
+
+        <!-- schedule view -->
+        <div class="form-group mt-4">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Schedule View</label>
+            <div class="col-sm-10">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleView"
+                  type="radio"
+                  id="view1"
+                  name="views"
+                  value="user"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="view1"
+                >Employee</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleView"
+                  type="radio"
+                  id="view2"
+                  name="views"
+                  value="job"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="view2"
+                >Jobs</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- schedule range -->
+        <div class="form-group">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">Schedule Range</label>
+            <div class="col-sm-10">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleRange"
+                  type="radio"
+                  id="range1"
+                  name="ranges"
+                  value="Week"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="range1"
+                >Week</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.scheduleRange"
+                  type="radio"
+                  id="range2"
+                  name="ranges"
+                  value="Month"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="range2"
+                >Month</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- ical -->
+        <div class="form-group">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">ICal</label>
+            <div class="col-sm-10">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.ical"
+                  type="radio"
+                  id="icalEnable"
+                  name="ical"
+                  :value="true"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="icalEnable"
+                >Enable</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input
+                  v-model="form.ical"
+                  type="radio"
+                  id="icalDisable"
+                  name="ical"
+                  :value="false"
+                  class="custom-control-input"
+                >
+                <label
+                  class="custom-control-label"
+                  for="icalDisable"
+                >Disable</label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- ical url -->
+        <div class="form-group">
+          <div class="form-row">
+            <label class="col-sm-2 col-form-lable">ICal URL</label>
+            <div class="col-sm-10">
+              <div class="input-group mb-3">
+                <input
+                  v-model="form.icalUrl"
+                  type="text"
+                  class="form-control"
+                  placeholder="Enable ICal to recive the URL"
+                  aria-label="ical url"
+                  aria-describedby="button-addon2"
+                >
+                <div class="input-group-append">
+                  <button
+                    class="btn btn-secondary"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    <ts-icon
+                      icon="file_copy"
+                      style="font-size:inherit;"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </template>
+  </app-main>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import pageMixin from "../../../../mixins/page-mixin";
+
+export default {
+  name: "UserSettings",
+
+  mixins: [pageMixin],
+
+  data() {
+    return {
+      themes: [
+        { key: 0, value: "Default" },
+        { key: 1, value: "Urban" },
+        { key: 2, value: "Jungle" },
+        { key: 3, value: "Beach" },
+        { key: 4, value: "Night" }
+      ],
+      form: {
+        theme: {},
+        ical: false,
+        icalUrl: "",
+        scheduleView: "user",
+        scheduleRange: "Month"
+      }
+    };
+  },
+
+  computed: {
+    ...mapGetters({
+      userId: "user/getId",
+    })
+  },
+
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      vm.loadAssets();
+    });
+  },
+
+  beforeRouteLeave(to, from, next) {
+    this.clear("user");
+    next();
+  },
+
+  methods: {
+    ...mapActions({
+      setTheme: "core/theme",
+      list: "resource/list",
+      update: "resource/update",      
+      clear: "resource/clearResource",
+      updateSettings: "user/updateSettings",
+    }),
+
+    loadAssets() {
+      this.startLoading();
+      this.list({ resource: "user", query: { eq: `id,${this.userId}` } }).then(
+        data => {
+          this.populateForm(data[0].meta);
+          this.stopLoading();
+        }
+      );
+    },
+
+    populateForm(settings) {      
+      this.form.theme = this.themes.find(
+        item => item.value.toLowerCase() === settings.theme
+      );
+      this.setTheme(this.form.theme.value);
+      this.form.scheduleView = settings.schedule_view;
+      this.form.scheduleRange = settings.schedule_range;
+      this.form.ical = settings.ical ? true : false;
+      this.form.icalUrl = settings.ical
+        ? `https://${window.location.hostname}/api/ical/${this.userId}/${settings.ical}`
+        : "";
+    },
+
+    onSave(e) {
+      e.preventDefault();
+      e.target.disable = true;
+
+      // setup the req body
+      let form = {};
+      form.theme = this.form.theme.value.toLowerCase();
+      form.ical = this.form.ical;
+      form.schedule_view = this.form.scheduleView;
+      form.schedule_range = this.form.scheduleRange;
+
+      this.update({ resource: "user", id: this.userId, data: form })
+        .then(data => {
+          this.populateForm(data.meta);
+          this.updateSettings(data.meta);
+          this.showMessage(`Settings updated successfuly.`, "success");
+        })
+        .catch(error => {
+          this.showMessage(error.message, "danger");
+        })
+        .finally(() => {
+          e.target.disable = false;
+        });
+    },
+
+    onCancel(e) {
+      e.preventDefault();
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
