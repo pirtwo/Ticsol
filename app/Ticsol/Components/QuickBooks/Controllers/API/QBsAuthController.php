096f9880a511b4370f78e848479bf86a29ef798d
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-10-18 17:53:17 +0330

    seperate QBs auth from api

---
+++ app/Ticsol/Components/QuickBooks/Controllers/API/QBsAuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Ticsol\Components\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Ticsol\Components\QuickBooks\Classes\QBsAuth;
+use Illuminate\Http\Request;
+
+class QBsAuthController extends Controller
+{
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = array(
+            'auth_mode' => env("QUICKBOOKS_AUTH_MODE"),
+            'ClientID' => env("QUICKBOOKS_CLIENT_ID"),
+            'ClientSecret' => env("QUICKBOOKS_CLIENT_SECRET"),
+            'RedirectURI' => env("QUICKBOOKS_REDIRECT_URI"),
+            'scope' => env("QUICKBOOKS_SCOPE"),
+            'baseUrl' => env("QUICKBOOKS_BASE_URL"),
+        );
+    }
+
+    public function token(Request $req, $code, $realmid = null)
+    {
+        $client = $req->user()->client;
+        $qbsAuth = new QBsAuth($client, $this->config);
+
+        $token = $qbsAuth->exchangeAuthCodeForToken($code, $realmid);
+
+        return \response()->json(["success" => true], 200);
+    }
+
+    public function refresh(Request $req)
+    {
+        $client = $req->user()->client;
+
+        $auth = new QBsAuth($this->config, $token, $token["realmid"]);
+
+        $auth->updateAccessToken();
+        $token = $auth->accessTokenToArray();
+        $client->saveQBsToken($token);
+
+        return $token;
+    }
+
+    public function hasValidToken(Request $req)
+    {
+        $client = $req->user()->client;
+
+        if (!$client->hasQBsToken()) {
+            return \response()->json([
+                "success" => true,
+                "isTokenValid" => false,
+            ], 200);
+        }
+
+        $token = $client->getQBsToken();
+
+        $auth = new QBsAuth($this->config, $token, $token["realmid"]);
+
+        return \response()->json([
+            "success" => true,
+            "isTokenValid" => !($auth->isTokenExpired() && $auth->isRefreshTokenExpired()),
+        ], 200);
+    }
+}
--- app/Ticsol/Components/QuickBooks/Controllers/API/QuickBooksController.php
+++ app/Ticsol/Components/QuickBooks/Controllers/API/QuickBooksController.php
@@ -3,57 +3,136 @@
 namespace App\Ticsol\Components\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use QuickBooksOnline\API\Core\OAuth\OAuth2\OAuth2AccessToken;
-use QuickBooksOnline\API\DataService\DataService;
+use App\Ticsol\Components\QuickBooks\Classes\QBsAPI;
 use App\Ticsol\Components\QuickBooks\Classes\QBsAuth;
+use Illuminate\Http\Request;
 
 class QuickBooksController extends Controller
 {
-    private $config;
+    protected $config;
 
     public function __construct()
     {
         $this->config = array(
-            'auth_mode'     => env("QUICKBOOKS_AUTH_MODE"),
-            'ClientID'      => env("QUICKBOOKS_CLIENT_ID"),
-            'ClientSecret'  => env("QUICKBOOKS_CLIENT_SECRET"),
-            'RedirectURI'   => env("QUICKBOOKS_REDIRECT_URI"),
-            'scope'         => env("QUICKBOOKS_SCOPE"),
-            'baseUrl'       => env("QUICKBOOKS_BASE_URL"),
+            'auth_mode' => env("QUICKBOOKS_AUTH_MODE"),
+            'ClientID' => env("QUICKBOOKS_CLIENT_ID"),
+            'ClientSecret' => env("QUICKBOOKS_CLIENT_SECRET"),
+            'RedirectURI' => env("QUICKBOOKS_REDIRECT_URI"),
+            'scope' => env("QUICKBOOKS_SCOPE"),
+            'baseUrl' => env("QUICKBOOKS_BASE_URL"),
         );
-    }
-
-    public function token(Request $req, $code, $realmid = null)
-    {
-        $client = $req->user()->client;
-        $qbAuth = new QBsAuth($this->config);
-
-        $token = $qbAuth->exchangeAuthCodeForToken($code, $realmid);
-
-        // save token for client
-        $settings = $client->meta;
-        $settings["quickbooks"] = $token;
-        $client->meta = $settings;
-        $client->save();
-
-        return \response()->json(["success" => true], 200);
     }    
 
-    public function getCompanyInfo(Request $req)
+    /**
+     * method: GET
+     * 
+     * return the QBs company informations.
+     */
+    public function companyInfo(Request $req)
     {
         $client = $req->user()->client;
-        $token = $this->clientAccessToken($client);
-        $qbAuth = new QBsAuth($this->config, $token, $token["realmid"]);
 
-        $dataService = $qbAuth->getDataService();
-        $companyInfo = $dataService->getCompanyInfo();
+        $api = $this->createQBsAPI($client);
 
-        return \json_encode($companyInfo);
+        return \json_encode($api->getCompanyInfo());
     }
 
-    private function clientAccessToken($client)
+    /**
+     * method: GET
+     * 
+     * return the QBs employees list.
+     */
+    public function employeeList(Request $req)
     {
-        return $client->meta["quickbooks"];
+        $client = $req->user()->client;
+
+        $api = $this->createQBsAPI($client);
+
+        return \json_encode($api->getEmployees());
+    }
+
+    /**
+     * method: GET
+     * 
+     * return the QBs cutomers list.
+     */
+    public function customerList(Request $req)
+    {
+        $client = $req->user()->client;
+
+        $api = $this->createQBsAPI($client);
+
+        return \json_encode($api->getCustomers());
+    }
+
+    /**
+     * method: GET
+     * 
+     * return the QBs class list.
+     */
+    public function classList(Request $req)
+    {
+        $client = $req->user()->client;
+
+        $api = $this->createQBsAPI($client);
+
+        return \json_encode($api->getClasses());
+    }
+
+    /**
+     * method: GET
+     * 
+     * return QBs departments(locations) list.
+     */
+    public function departmentList(Request $req)
+    {
+        $client = $req->user()->client;
+
+        $api = $this->createQBsAPI($client);
+
+        return \json_encode($api->getDepartments());
+    }  
+    
+    // ===== helpers =====
+
+    /**
+     * creates instance of QBsAPI class.
+     * 
+     * @param \App\Ticsol\components\Models\Client $client  
+     * @return \App\Ticsol\components\QuickBooks\Classes\QBsAPI
+     */
+    protected function createQBsAPI($client)
+    {
+        $auth = $this->createAuth($client);
+
+        return new QBsAPI($auth);
+    }
+
+    /**
+     * creates instance of QBsAuth class.
+     * 
+     * @param \App\Ticsol\components\Models\Client $client 
+     * @return \App\Ticsol\components\QuickBooks\Classes\QBsAuth 
+     */
+    protected function createAuth($client)
+    {
+        if ($client->hasQBsToken()) {
+            $token = $client->getQBsToken();
+            $auth = new QBsAuth($this->config, $token, $token["realmid"]);
+
+            if ($auth->isTokenExpired()) {
+                if ($auth->isRefreshTokenExpired()) {
+                    throw new \Exception("refresh token is expired, plaease login again to QBs service.");
+                } else {
+                    $auth->updateAccessToken();
+                    $token = $auth->accessTokenToArray();
+                    $client->saveQBsToken($token);
+                }
+            }
+        } else {
+            throw new Exception("Error, not authenticated to QBs.", 1);
+        }
+
+        return $auth;
     }
 }
