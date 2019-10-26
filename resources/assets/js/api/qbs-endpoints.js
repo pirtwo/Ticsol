e769c8e1d029943a228f7b7b5afa8787da4e6aab
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-10-26 19:16:26 +0330

    add QBs endpoints list

---
+++ resources/assets/js/api/qbs-endpoints.js
@@ -0,0 +1,27 @@
+/** 
+ * method: GET
+ * 
+ * base url for exchange code for token. 
+ */
+export const TOKEN = '/api/quickbooks/token/';
+
+/** 
+ * method: GET
+ * 
+ * create refresh token. 
+ */
+export const REFRESH = '/api/quickbooks/refresh';
+
+/** 
+ * method: GET
+ * 
+ * check current client QBs token. 
+ */
+export const CHECK_TOKEN = '/api/quickbooks/hasvalidtoken';
+
+export const EMPLOYEE = '/api/quickbooks/employee';
+export const VENDOR = '/api/quickbooks/vendor';
+export const CUSTOMER = '/api/quickbooks/customer';
+export const CLASS = '/api/quickbooks/class';
+export const DEPARTMENT = '/api/quickbooks/department';
+export const ACCOUNT = '/api/quickbooks/account';
