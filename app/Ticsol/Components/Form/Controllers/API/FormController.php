92dfaa8e1ba6bc004b6364666e3eddac24cc963b
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-11-17 11:07:32 +0330

    move rules logic to form-requests

--- app/Ticsol/Components/Form/Controllers/API/FormController.php
+++ app/Ticsol/Components/Form/Controllers/API/FormController.php
@@ -9,11 +9,9 @@ use App\Ticsol\Base\Exceptions\NotFound;
 use App\Ticsol\Components\Form\Events;
 use App\Ticsol\Components\Form\Repository;
 use App\Ticsol\Components\Form\Requests;
-use App\Ticsol\Components\Form\Rules;
 use App\Ticsol\Components\Models\Form;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
 
 class FormController extends Controller
 {
@@ -121,17 +119,6 @@ class FormController extends Controller
         //      AUTHORIZE ACTION
         //----------------------------
         $this->authorize('update', $form);
-        $parent_id = $request->input('parent_id', null);
-
-        $this->repository->newQuery();
-        $parent = $this->repository->findBy('id', $parent_id);
-        Validator::make(
-            [
-                'parent_id' => $request->input('parent_id', null),
-            ],
-            [
-                'parent_id' => [new Rules\HierarchyDepth($form, $parent, 3), new Rules\HierarchyCycle($form)],
-            ])->validate();
 
         $form->update($request->all());
 
