903d2ca450ed4fc2df2e351da16f458bb659cf0a
Ahmad-Fz <ahmad.f.zadeh@icloud.com>
2019-12-08 10:28:22 +0330

    add section to unav-hours

--- resources/assets/js/components/pages/dashboard/users/UserProfile.vue
+++ resources/assets/js/components/pages/dashboard/users/UserProfile.vue
@@ -200,27 +200,25 @@
             </div>
           </div>
         </fieldset>
-      </form>
-
-      <br>
-      <hr>
-
-      <!-- Unavailable hours calendar -->
-      <ts-datescroller
-        v-model="week"
-        range="Week"
-        :week-start="'Sun'"
-        @change="loadCalendar"
-      />
-      <calendar
-        class="mb-5"
-        :start-date="weekStart"
-        :events="unavailables"
-        :view-type="'Weeks'"
-        :disabled="id != userId"
-        @range-selected="onRangeSelect"
-        @event-clicked="onEventClicked"
-      />
+      </form>      
+      
+      <ts-section title="Unavailable Hours" :devider="false" class="mb-2">
+        <!-- Unavailable hours calendar -->
+        <ts-datescroller
+          v-model="week"
+          range="Week"
+          :week-start="'Sun'"
+          @change="loadCalendar"
+        />
+        <calendar          
+          :start-date="weekStart"
+          :events="unavailables"
+          :view-type="'Weeks'"
+          :disabled="id != userId"
+          @range-selected="onRangeSelect"
+          @event-clicked="onEventClicked"
+        />
+      </ts-section>
 
       <!-- Create Event Modal -->
       <ts-modal
